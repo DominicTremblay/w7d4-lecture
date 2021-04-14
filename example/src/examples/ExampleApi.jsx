bc8cfd4865179b194c3cbbd54b254d0d6802572f
Dominic Tremblay <dominictremblay24@gmail.com>
2021-04-14 09:39:12 -0400

    changed the ouput of the api

--- example/src/examples/ExampleApi.jsx
+++ example/src/examples/ExampleApi.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.css';
 import './exampleApi.css';
 
 const ExampleApi = (props) => {
   const [loading, setLoading] = useState(true);
   const [err, setErr] = useState(false);
   const [heros, setHeros] = useState([]);
-  const [heroId, setHeroId] = useState(1);
+  const [heroId, setHeroId] = useState(Math.floor(Math.random() * 300) + 1);
 
   useEffect(() => {
     axios({
@@ -14,6 +15,7 @@ const ExampleApi = (props) => {
       url: `/api/${process.env.REACT_APP_API_TOKEN}/${heroId}`,
     })
       .then((result) => {
+        console.log(result.data);
         setHeros((prev) => [...prev, result.data]);
         setLoading(false);
         setErr('');
@@ -25,9 +27,34 @@ const ExampleApi = (props) => {
   }, [heroId]);
 
   const superheros = heros.map((hero) => (
-    <li className="hero" key={hero.id}>
+    <div
+      className="card"
+      style={{ width: '18rem' }}
+      className="hero"
+      key={hero.id}
+    >
       <img src={hero.image.url} alt={hero.name} /> {hero.name}
-    </li>
+      <div className="card-body">
+        <p className="card-text">
+          <h3>Power Stats</h3>
+          <ul className="list-group">
+            <li className="list-group-item">
+              Combat: {hero.powerstats.combat}
+            </li>
+            <li className="list-group-item">
+              Durability: {hero.powerstats.durability}
+            </li>
+            <li className="list-group-item">
+              Power: {hero.powerstats.durability}
+            </li>
+            <li className="list-group-item">Speed: {hero.powerstats.speed}</li>
+            <li className="list-group-item">
+              Strenght: {hero.powerstats.strength}
+            </li>
+          </ul>
+        </p>
+      </div>
+    </div>
   ));
 
   const handleClick = (evt) => {
@@ -37,7 +64,7 @@ const ExampleApi = (props) => {
   };
 
   return (
-    <>
+    <div className="container">
       <h1>SuperHero API</h1>
       {loading && <h3>Loading...</h3>}
 
@@ -46,7 +73,7 @@ const ExampleApi = (props) => {
       {!loading && (
         <input type="button" value="Pick a hero" onClick={handleClick} />
       )}
-    </>
+    </div>
   );
 };
 
